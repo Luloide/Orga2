3c4589a0a533a8819ac6323145562c0fd35aad8d
Luloide <lucia.silva.alberto@gmail.com>
2025-05-01 19:35:41 -0300

    imma go to bed

--- primeros_parciales_viejos/2023C1R/solucion/ej1/ej1.asm
+++ primeros_parciales_viejos/2023C1R/solucion/ej1/ej1.asm
@@ -29,14 +29,16 @@ acumuladoPorCliente_asm:
 	cmp R8, RBX
 	je .terminar
 	; R12 es mi pago actual
+	xor R9,R9; limpio el R9
+	xor R10,R10; limpio el R10
 	mov R9B, [R12 + 17] ; ragoActual.aprobado
 	cmp R9B, 0
 	je .proximaIteracion ; si es falso continue
 	mov R9B, [R12 + 16] ; ragoActual.cliente
 	imul R9, 4
-	mov R9B, [R12] ;PagoActual.monto
-	add [RAX + R9], R9B
-	
+	mov R10B, [R12] ;PagoActual.monto
+	add [RAX + R9], R10B
+
 .proximaIteracion:
 	add R12, 24; agarro el proximo elemento de la lista de pagos
 	inc R8
@@ -50,7 +52,61 @@ acumuladoPorCliente_asm:
 	ret
 
 en_blacklist_asm:
+	;prolgo
+	push RBP
+	mov RBP, RSP
+	xor RAX, RAX; inicializo res como 0
+	xor R8, R8; i=0
+
+.loop:	
+	cmp R8, RDX
+	je .terminar
+	mov R9, [RSI + R8]
+	cmp R9, RDI
+	jne .proximaIteracion
+	inc RAX
+
+.proximaIteracion:
+	inc R8
+	jmp .loop
+
+.terminar:
+	;Epilogo
+	pop RBP
 	ret
 
 blacklistComercios_asm:
+	; primero contar los pagos que hicieron los comercioes en blacklist para pedir la memoria encesaria
+	;prolgo
+	push RBP
+	mov RBP, RSP
+	push R12
+	push R13 ;alineado
+	push R14
+	push R15 ; alineado
+
+	;me guardo mis parametros en registros no volatiles
+	mov R12, RDI ; comercio
+	mov R13, RSI ;lista comercio
+	mov R14, RDX ; n (cant comercios en lista)
+	
+	xor RAX, RAX; inicializo res como 0
+
+.bublecantPagos:
+	cmp R15, R14
+	je .guardarPagos
+	mov RDI
+	mov RSI
+
+.guardarPagos:
+
+
+.terminar:
+	;Epilogo
+	pop R15
+	pop R14
+	pop R13
+	pop R12
+	pop RBP
 	ret
+
--- primeros_parciales_viejos/2023C1R/solucion/ej1/ej1.c
+++ primeros_parciales_viejos/2023C1R/solucion/ej1/ej1.c
@@ -16,9 +16,35 @@ uint32_t* acumuladoPorCliente(uint8_t cantidadDePagos, pago_t* arr_pagos){
 }
 
 uint8_t en_blacklist(char* comercio, char** lista_comercios, uint8_t n){
+    int res = 0; // inicializo como que no esta en la lista
+    for(int i = 0; i < n; i++){
+        if(comercio != lista_comercios[i]){
+            continue;
+        }
+        res = 1;
+    }
+    return res;
+
 }
 
 pago_t** blacklistComercios(uint8_t cantidad_pagos, pago_t* arr_pagos, char** arr_comercios, uint8_t size_comercios){
+    // primero deberia contar cuantos pagos fueron hechos por comercions en blacklist asi pido bien la memoria
+    int pagos = 0;
+    for(int i = 0; i < cantidad_pagos; i++){
+        if(en_blacklist(arr_pagos[i].comercio, arr_comercios, size_comercios)){
+            pagos++;
+        }
+    }
+    pago_t **pagosBlacklist = calloc(pagos, sizeof(pago_t));
+    int j = 0;
+    //itero sobre los pagos devuelta y los que son blacklist lo agrego
+    for(int i = 0; i < cantidad_pagos; i++){
+        if(en_blacklist(arr_pagos[i].comercio, arr_comercios, size_comercios)){
+            pagosBlacklist[j] = &arr_pagos[i];
+            j++;
+        }
+    }
+    return pagosBlacklist;
 }
 
 
